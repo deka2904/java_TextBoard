75e91e39c511b87862c4253516e9182955467656
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-09-11 12:48:22 +0900

    0911 commit

--- src/main/java/Main_Text.java
+++ src/main/java/Main_Text.java
@@ -21,21 +21,21 @@ class Board {
         this.detail = newDetail;
     }
 }
+
 public class Main_Text {
     private static int number = 1;
-    public static int exam(int num){
-        ArrayList <Board> Boardlist = new ArrayList<>();
-        int index = -1;
-        for (int i = 0; i < Boardlist.size(); i++) {
-            if (Boardlist.get(i).getNumber() == num) {
-                index = i;
-                break;
+    private static final ArrayList<Board> boardList = new ArrayList<>();
+
+    // 수정, 삭제 공통 프로세스 공유
+    public static int findIndex(int num){
+        for (int i = 0; i < boardList.size(); i++) {
+            if (boardList.get(i).getNumber() == num) {
+                return i;
             }
         }
-        return index;
+        return -1;
     }
     public static void main(String[] args) {
-        ArrayList <Board> Boardlist = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         while(true) {
             System.out.print("입력 : ");
@@ -48,13 +48,13 @@ public class Main_Text {
                 System.out.print("게시물 내용을 입력해주세요 : ");
                 String detail = scanner.nextLine();
                 new_Board = new Board(number, title, detail);
-                Boardlist.add(new_Board);
+                boardList.add(new_Board);
                 System.out.println("게시물이 등록되었습니다.");
                 number++;
             }
             else if (func.equals("list")) {
-                for (Board board : Boardlist) {
-                    System.out.println("==================");
+                System.out.println("==================");
+                for (Board board : boardList) {
                     System.out.println("번호 : " + board.number);
                     System.out.println("제목 : " + board.title);
                     System.out.println("내용 : " + board.detail);
@@ -65,16 +65,16 @@ public class Main_Text {
                 System.out.print("수정할 게시물 번호 : ");
                 try{
                     int num = Integer.parseInt(scanner.nextLine());
-                    int index = exam(num);
+                    int index = findIndex(num);
                     if (index != -1) {
                         System.out.print("새로운 게시물 제목을 입력해주세요 : ");
                         String newTitle = scanner.nextLine();
                         System.out.print("새로운 게시물 내용을 입력해주세요 : ");
                         String newDetail = scanner.nextLine();
 
-                        Boardlist.get(index).setTitle(newTitle);   // 수정할 게시글 제목
-                        Boardlist.get(index).setDetail(newDetail); // 수정할 게시글 내용
-                        System.out.printf("%d번 게시물이 수정되었습니다.\n", index);
+                        boardList.get(index).setTitle(newTitle);   // 수정할 게시글 제목
+                        boardList.get(index).setDetail(newDetail); // 수정할 게시글 내용
+                        System.out.printf("%d번 게시물이 수정되었습니다.\n", num);
                     } else {
                         System.out.println("없는 게시물 번호입니다.");
                     }
@@ -86,15 +86,15 @@ public class Main_Text {
                 System.out.print("삭제할 게시물 번호 : ");
                 try {
                     int num = Integer.parseInt(scanner.nextLine());
-                    int index = exam(num);
+                    int index = findIndex(num);
                     if (index != -1) {
-                        Boardlist.remove(index);
+                        boardList.remove(index);
                         System.out.printf("%d번 게시물이 삭제되었습니다.\n", num);
                     } else {
                         System.out.println("없는 게시물 번호입니다.");
                     }
-                    for (Board board : Boardlist) {
-                        System.out.println("==================");
+                    System.out.println("==================");
+                    for (Board board : boardList) {
                         System.out.println("번호 : " + board.number);
                         System.out.println("제목 : " + board.title);
                         System.out.println("내용 : " + board.detail);

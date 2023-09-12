23b8a7ba2da7a6e045e063dd1a713078e6c52a6c
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-09-12 16:01:09 +0900

    0912 commit

--- src/main/java/CreateTable.java
+++ src/main/java/CreateTable.java
@@ -10,6 +10,7 @@ public class CreateTable {
             try{
                 statement = connection.createStatement();
 
+                // 게시판 테이블
                 String createTableSQL = "CREATE TABLE IF NOT EXISTS text_board (" +
                         "number INT AUTO_INCREMENT PRIMARY KEY," +
                         "title VARCHAR(255)," +
@@ -17,16 +18,27 @@ public class CreateTable {
                         "view_count INT DEFAULT 0," +
                         "time TIMESTAMP DEFAULT NOW())";
 
+                // 댓글 테이블
                 String createCommentTableSQL = "CREATE TABLE IF NOT EXISTS text_board_comment (" +
                         "id INT AUTO_INCREMENT PRIMARY KEY," +
                         "comment TEXT," +
-                        "suggestion INT DEFAULT 0," +
-                        "time TIMESTAMP DEFAULT NOW())";
+                        "comment_suggestion INT DEFAULT 0," +
+                        "comment_time TIMESTAMP DEFAULT NOW()," +
+                        "board_number INT," + // 외래 키 추가
+                        "FOREIGN KEY (board_number) REFERENCES text_board(number))"; // 외래 키 제약 조건 추가
+
+                // 회원 테이블
+                String createMemberTableSQL = "CREATE TABLE IF NOT EXISTS member("+
+                        "id VARCHAR(255) PRIMARY KEY," +
+                        "password VARCHAR(255)," +
+                        "nickname VARCHAR(255))";
 
                 statement.executeUpdate(createTableSQL);
                 statement.executeUpdate(createCommentTableSQL);
+                statement.executeUpdate(createMemberTableSQL);
                 System.out.println("createTableSQL Table created successfully");
                 System.out.println("createCommentTableSQL Table created successfully");
+                System.out.println("createMemberTableSQL Table created successfully");
 
                 statement.close();
                 connection.close();
---
+++ src/main/java/Sign.java
@@ -0,0 +1,122 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Sign {
+    Scanner scanner = new Scanner(System.in);
+    Connection connection = null;
+    PreparedStatement idCheckStatement = null;
+    PreparedStatement preparedStatement = null;
+
+    // 회원가입
+    public void sign_in() {
+        System.out.println("sign_in");
+
+        System.out.print("아이디를 입력해주세요: ");
+        String id = scanner.nextLine();
+
+        try {
+            // JDBC 연결 설정
+            connection = DatabaseConnection.getConnection();
+
+            // 아이디 중복 체크
+            String idCheckQuery = "SELECT id FROM member WHERE id = ?";
+            idCheckStatement = connection.prepareStatement(idCheckQuery);
+            idCheckStatement.setString(1, id);
+
+            ResultSet resultSet = idCheckStatement.executeQuery();
+
+            if (resultSet.next()) {
+                // 아이디가 이미 존재하는 경우
+                System.out.println("[[존재하는 id입니다.]]");
+            } else {
+                // 아이디가 존재하지 않는 경우
+                System.out.println("[[아이디를 사용할 수 있습니다.]]");
+
+                while (true){
+                    System.out.print("패스워드를 입력해주세요: ");
+                    String pw1 = scanner.nextLine();
+                    System.out.print("입력하신 패스워드를 재확인 합니다: ");
+                    String pw2 = scanner.nextLine();
+                    System.out.print("닉네임을 입력하세요: ");
+                    String nickname = scanner.nextLine();
+                    if (pw1.equals(pw2)){
+                        // SQL 쿼리를 사용하여 데이터베이스에 회원 정보 추가
+                        String insertQuery = "INSERT INTO member (id, password, nickname) VALUES (?, ?, ?)";
+                        preparedStatement = connection.prepareStatement(insertQuery);
+                        preparedStatement.setString(1, id);
+                        preparedStatement.setString(2, pw1);
+                        preparedStatement.setString(3, nickname);
+
+                        int rowsAffected = preparedStatement.executeUpdate();
+
+                        if (rowsAffected > 0) {
+                            System.out.println("[[회원 가입이 완료되었습니다.]]");
+                            break;
+                        }
+                    }else{
+                        System.out.println("[[패스워드를 잘못입력하셨습니다.]]");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // 사용한 자원 해제
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (idCheckStatement != null) {
+                    idCheckStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    // 로그인
+    public String login(){
+        System.out.println("login");
+
+        System.out.print("아이디를 입력해주세요: ");
+        String id = scanner.nextLine();
+        System.out.print("패스워드를 입력해주세요: ");
+        String pw = scanner.nextLine();
+        try {
+            // JDBC 연결 설정
+            connection = DatabaseConnection.getConnection();
+
+            // 아이디와 패스워드로 회원 조회
+            String memberCheckQuery = "SELECT * FROM member WHERE id = ? AND password = ?";
+            PreparedStatement memberCheckStatement = connection.prepareStatement(memberCheckQuery);
+            memberCheckStatement.setString(1, id);
+            memberCheckStatement.setString(2, pw);
+
+            ResultSet resultSet = memberCheckStatement.executeQuery();
+
+            if (resultSet.next()) {
+                // 로그인 성공
+                String nickname = resultSet.getString("nickname"); // 닉네임을 가져옴
+                System.out.println("로그인 성공!");
+                return nickname;
+            } else {
+                // 로그인 실패: 아이디 또는 패스워드가 일치하지 않음
+                System.out.println("존재하지 않은 회원입니다.");
+                return "";
+            }
+        } catch (Exception e) {
+            System.out.println("오류 발생: " + e.getMessage());
+            return "";
+        }
+    }
+    // 로그아웃
+    public void logout(){
+        System.out.println("logout");
+    }
+}

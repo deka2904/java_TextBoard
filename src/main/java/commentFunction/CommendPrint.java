2b6351d795a1628bbf3455c9ced379ce5e7f3771
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-09-21 11:27:19 +0900

    0921 commit

--- src/main/java/commentFunction/CommendPrint.java
+++ src/main/java/commentFunction/CommendPrint.java
@@ -6,24 +6,29 @@ import java.util.List;
 
 public class CommendPrint {
     public void commendPrint(List<Commend> CommendList) {
-        if (CommendList.size() == 0) {
-            System.out.println("등록된 댓글이 없습니다.");
-        } else {
-            System.out.println("=============================================================================");
-            for (Commend commends : CommendList) {
-                String commend = commends.getCommend();
-                String comment_member_nickname = commends.getComment_member_nickname();
-                String time = commends.getTime();
+        boolean hasValidCommends = false;
+        System.out.println("=============================================================================");
+        for (Commend commends : CommendList) {
+            String commend = commends.getCommend();
+            String comment_member_nickname = commends.getComment_member_nickname();
+            String time = commends.getTime();
 
+            // commend, comment_member_nickname, time 중 하나라도 null이면 해당 댓글을 출력하지 않음
+            if (commend != null && comment_member_nickname != null && time != null) {
                 // 가져온 결과를 출력
                 System.out.println("[댓글] : " + commend);
                 System.out.println("[작성자] : " + comment_member_nickname);
                 System.out.println("[시간] : " + time);
                 System.out.println("=============================================================================");
+                hasValidCommends = true;
             }
+        }
+        if (!hasValidCommends) {
+            System.out.println("해당 게시글에 유효한 댓글이 없습니다.");
         }
     }
 }
+
 
 
 
--- src/main/java/commentFunction/CommendQueryManager.java
+++ src/main/java/commentFunction/CommendQueryManager.java
@@ -6,7 +6,8 @@ public class CommendQueryManager {
     Commend_Action commend_action = new Commend_Action();
     // 해당 게시글 닉네임으로 댓글 조회
     public List<Commend> getAllCommend(int number) {
-        String sql = "SELECT comment, comment_time, comment_member_nickname FROM text_board LEFT JOIN text_board_comment ON text_board.number = text_board_comment.board_number WHERE text_board.number = " + number;
+        // comment, comment_time, comment_member_nickname
+        String sql = "SELECT *  FROM text_board LEFT JOIN text_board_comment ON text_board.number = text_board_comment.board_number WHERE text_board.number = " + number;
         List<Commend> articleList = commend_action.selectOne(sql);
 
         return articleList;
